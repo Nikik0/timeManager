4653b7e0df449c380532c6243981872a0c3d2dfd
Nikita <nikitapolin47@gmail.com>
2023-07-07 12:06:14 +1000

    clean testing for findById in userService

--- src/test/java/com/nikiko/timemanager/serviceTest/UserServiceTest.java
+++ src/test/java/com/nikiko/timemanager/serviceTest/UserServiceTest.java
@@ -32,73 +32,31 @@ import java.util.logging.Logger;
 @Slf4j
 @ExtendWith(SpringExtension.class)
 public class UserServiceTest {
-
-//    @Mock
-//    private UserResponseMapper userResponseMapper;
-//    @Mock
-//    private UserRequestMapper userRequestMapper;
     @InjectMocks
     private UserService userService;
     @Mock
     private UserRepository userRepository;
-    @MockBean
-    private UserRequestMapper userRequestMapper;
-    @MockBean
-    private UserResponseMapper userResponseMapper;
-    @MockBean
-    private UserResponseMapperImpl userResponseMapperImp;
-    UserRequestDto USER_1 = new UserRequestDto();
-    UserRequestDto USER_2 = new UserRequestDto();
-    UserRequestDto USER_3 = new UserRequestDto();
+    @Spy
+    private UserResponseMapper userResponseMapper = Mappers.getMapper(UserResponseMapper.class);
     private UserEntity entity;
-
+    private UserDto userDto;
     @BeforeEach
     public void setup(){
-        entity = new UserEntity(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());
-        USER_1.setId(1L);
-        USER_1.setName("Neath");
-        USER_1.setPassword("qwerty");
-        USER_2.setId(100L);
-        USER_2.setName("Nerw");
-        USER_2.setPassword("qwerty321");
-        USER_3.setId(1L);
-        USER_3.setName("Neath");
-        USER_3.setPassword("qwerty");
-        //UserEntity userEntity_1 = userRequestMapper.mapFromRequestToEntity(USER_1);
-        System.out.println(entity);
-        BDDMockito.when(userRepository.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(Mono.just(entity));
-        BDDMockito.when(userRepository.save(entity))
-                .thenReturn(Mono.just(entity));
-        BDDMockito.when(userRepository.findAll())
-                .thenReturn(Flux.just(entity));
-        UserDto userDto = new UserDto(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());
-        Mockito.when(userResponseMapper.mapFromEntityToResponse(org.mockito.ArgumentMatchers.any())).thenReturn(userDto);
+        entity = new UserEntity(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());;
+        userDto = userResponseMapper.mapFromEntityToResponse(entity);
+        Mockito.when(userResponseMapper.mapFromEntityToResponse(entity)).thenReturn(userDto);
     }
-
 
     @Test
     public void findById_returnMonoUserDto_whenSuccessful() throws Exception{
-        entity = new UserEntity(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());
-        BDDMockito.when(userRepository.findById(ArgumentMatchers.anyLong()))
+        BDDMockito.when(userRepository.findById(entity.getId()))
                 .thenReturn(Mono.just(entity));
-       // UserDto userDto_1 = userResponseMapper.mapFromEntityToResponse(userRequestMapper.mapFromRequestToEntity(USER_1));
-        UserDto userDto = new UserDto(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());
-        Mockito.when(userResponseMapperImp.mapFromEntityToResponse(org.mockito.ArgumentMatchers.any())).thenReturn(userDto);
-        Mockito.when(userResponseMapper.mapFromEntityToResponse(org.mockito.ArgumentMatchers.any())).thenReturn(userDto);
-
-        UserResponseMapper mapper = Mappers.getMapper(UserResponseMapper.class);
-        //Mockito.when(UserResponseMapper.INSTANCE.mapFromEntityToResponse(entity)).thenReturn(userDto);
-        System.out.println(userDto);
-        System.out.println(entity);
-        System.out.println("call will be initiated");
-        System.out.println("test map" + userRepository.findById(1L).map(userResponseMapperImp::mapFromEntityToResponse).block());
-        System.out.println("called test " + userService.findByIdTest(1L).block());
-        //System.out.println("called " + userService.findById(1L).block());
         System.out.println("call happened");
-        StepVerifier.create(userRepository.findById(1L))
+        System.out.println("entity is " + entity);
+        System.out.println("dto is " + userDto);
+        StepVerifier.create(userService.findById(1L))
                 .expectSubscription()
-                .expectNext(entity)
+                .expectNext(userDto)
                 .verifyComplete();
     }
 }
