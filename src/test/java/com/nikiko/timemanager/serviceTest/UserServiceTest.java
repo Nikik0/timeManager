70912272ae49cb8f988332b6774b9838667736ef
Nikita <nikitapolin47@gmail.com>
2023-07-07 13:27:18 +1000

    unit test coverage for UserService

--- src/test/java/com/nikiko/timemanager/serviceTest/UserServiceTest.java
+++ src/test/java/com/nikiko/timemanager/serviceTest/UserServiceTest.java
@@ -2,34 +2,25 @@ package com.nikiko.timemanager.serviceTest;
 
 import com.nikiko.timemanager.dto.UserDto;
 import com.nikiko.timemanager.dto.UserRequestDto;
-import com.nikiko.timemanager.entity.SubscriberEntity;
 import com.nikiko.timemanager.entity.UserEntity;
 import com.nikiko.timemanager.entity.UserRole;
+import com.nikiko.timemanager.exception.ApiException;
 import com.nikiko.timemanager.mapper.UserRequestMapper;
 import com.nikiko.timemanager.mapper.UserResponseMapper;
-import com.nikiko.timemanager.mapper.UserResponseMapperImpl;
 import com.nikiko.timemanager.repository.UserRepository;
 import com.nikiko.timemanager.service.UserService;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
 import org.mockito.*;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
 import java.time.LocalDateTime;
-import java.util.logging.Logger;
 
-@AllArgsConstructor
-@Slf4j
 @ExtendWith(SpringExtension.class)
 public class UserServiceTest {
     @InjectMocks
@@ -38,25 +29,66 @@ public class UserServiceTest {
     private UserRepository userRepository;
     @Spy
     private UserResponseMapper userResponseMapper = Mappers.getMapper(UserResponseMapper.class);
+    @Spy
+    private UserRequestMapper userRequestMapper = Mappers.getMapper(UserRequestMapper.class);
     private UserEntity entity;
     private UserDto userDto;
+    private UserRequestDto userRequestDto;
     @BeforeEach
     public void setup(){
-        entity = new UserEntity(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());;
+        entity = new UserEntity(1L, "Reak", "qwerty", false, UserRole.USER, LocalDateTime.now(), LocalDateTime.now());
         userDto = userResponseMapper.mapFromEntityToResponse(entity);
+        userRequestDto = new UserRequestDto(entity.getId(), entity.getName(), entity.getPassword());
         Mockito.when(userResponseMapper.mapFromEntityToResponse(entity)).thenReturn(userDto);
+        Mockito.when(userRequestMapper.mapFromRequestToEntity(userRequestDto)).thenReturn(entity);
     }
 
     @Test
-    public void findById_returnMonoUserDto_whenSuccessful() throws Exception{
+    @DisplayName("findById returns Mono of UserDto if the user exists")
+    public void findById_returnMonoUserDto_whenSuccessful(){
         BDDMockito.when(userRepository.findById(entity.getId()))
                 .thenReturn(Mono.just(entity));
-        System.out.println("call happened");
-        System.out.println("entity is " + entity);
-        System.out.println("dto is " + userDto);
         StepVerifier.create(userService.findById(1L))
                 .expectSubscription()
                 .expectNext(userDto)
                 .verifyComplete();
     }
+    @Test
+    @DisplayName("findById returns Mono error if the user wasn't found")
+    public void findById_returnMonoError_whenMonoEmpty(){
+        BDDMockito.when(userRepository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Mono.empty());
+        StepVerifier.create(userService.findById(1L))
+                .expectSubscription()
+                .expectError(ApiException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("save returns userDto when successful")
+    public void save_returnUserDto_whenSuccessful(){
+        BDDMockito.when(userRepository.save(Mockito.any(UserEntity.class)))
+                .thenReturn(Mono.just(entity));
+        StepVerifier.create(userService.save(userRequestDto))
+                .expectSubscription()
+                .expectNext(userDto)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("change returns changed userDto when successful")
+    public void change_returnUserDto_whenSuccessful(){
+        userRequestDto.setName("Changed Name");
+        userDto.setName(userRequestDto.getName());
+        BDDMockito.when(userRepository.findById(entity.getId()))
+                .thenReturn(Mono.just(entity));
+        UserEntity entity_2 = entity.toBuilder().name(userRequestDto.getName()).build();
+        BDDMockito.when(userRepository.save(Mockito.any(UserEntity.class)))
+                .thenReturn(Mono.just(entity_2));
+        StepVerifier.create(userService.change(userRequestDto))
+                .expectSubscription()
+                .expectNext(userDto)
+                .verifyComplete();
+    }
+
 }
